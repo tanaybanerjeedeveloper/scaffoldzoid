cab171713e8e6dff4dd9f820fe92b628658b0937
tanay_123 <tanay@tortalabz.com>
2022-09-09 22:42:31 +0530

    cmt

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -15,6 +16,7 @@ class LogInScreen extends StatefulWidget {
 }
 
 class _LogInScreenState extends State<LogInScreen> {
+  final formKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
@@ -26,58 +28,70 @@ class _LogInScreenState extends State<LogInScreen> {
           child: Padding(
             padding: const EdgeInsets.all(17.0),
             child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // SizedBox(
-                  //   height: 100,
-                  // ),
-                  TextField(
-                    controller: emailController,
-                    decoration: InputDecoration(labelText: 'Email'),
-                  ),
-                  SizedBox(
-                    height: 4,
-                  ),
-                  TextField(
-                    controller: passwordController,
-                    decoration: InputDecoration(labelText: 'Password'),
-                    obscureText: true,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.symmetric(vertical: 15),
-                      ),
-                      onPressed: signIn,
-                      child: Text('Log In'),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // SizedBox(
+                    //   height: 100,
+                    // ),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(labelText: 'Email'),
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (email) =>
+                          email != null && !EmailValidator.validate(email)
+                              ? 'Enter a valid email'
+                              : null,
                     ),
-                  ),
-                  SizedBox(height: 24),
-                  GestureDetector(
-                    onTap: () =>
-                        Navigator.pushNamed(context, SignUpScreen.routeName),
-                    child: RichText(
-                        text: TextSpan(
-                            text: 'No Account? ',
-                            style: TextStyle(color: Colors.black),
-                            children: [
-                          TextSpan(
-                              // recognizer: TapGestureRecognizer(),
-                              //   ..onTap: widget.onClickedSignUp,
-                              text: 'Sign Up',
-                              style: TextStyle(
-                                color: Colors.green,
-                                fontWeight: FontWeight.bold,
-                              ))
-                        ])),
-                  )
-                ],
+                    SizedBox(
+                      height: 4,
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      decoration: InputDecoration(labelText: 'Password'),
+                      obscureText: true,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (value) => value != null && value.length < 6
+                          ? 'Enter min 6 characters'
+                          : null,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(vertical: 15),
+                        ),
+                        onPressed: signIn,
+                        child: Text('Log In'),
+                      ),
+                    ),
+                    SizedBox(height: 24),
+                    GestureDetector(
+                      onTap: () =>
+                          Navigator.pushNamed(context, SignUpScreen.routeName),
+                      child: RichText(
+                          text: TextSpan(
+                              text: 'No Account? ',
+                              style: TextStyle(color: Colors.black),
+                              children: [
+                            TextSpan(
+                                // recognizer: TapGestureRecognizer(),
+                                //   ..onTap: widget.onClickedSignUp,
+                                text: 'Sign Up',
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  fontWeight: FontWeight.bold,
+                                ))
+                          ])),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
@@ -87,6 +101,9 @@ class _LogInScreenState extends State<LogInScreen> {
   }
 
   Future signIn() async {
+    final isValid = formKey.currentState!.validate();
+    if (!isValid) return;
+
     showDialog(
         context: context,
         barrierDismissible: false,
